c38d8ffa031828c0a0f5e51eb50720ec19a5d0bf
florastocks <stocksflora@gmail.com>
2022-09-20 15:24:49 +0100

    env added

--- project/settings.py
+++ project/settings.py
@@ -13,16 +13,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import django_on_heroku
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,8 +90,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
   'default': {
           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-          'NAME': 'recipe-api',
-          'HOST': 'localhost',
+          'NAME': env('recipe-api'),
+          'HOST': env('localhost'),
           'PORT': 5432
   }
 }
