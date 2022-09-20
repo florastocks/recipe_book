a83633d8812c67fbc93ca32b32ed0924d8d02104
florastocks <stocksflora@gmail.com>
2022-09-20 14:54:55 +0100

    changes

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-389r7=w#v5h-=1a9aa9cb7_grx68qk-^0mzr7_%q@i9*9vn!z='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
